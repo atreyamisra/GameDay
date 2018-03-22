16c6ecfbe8dfd35b329cc66f577c326277d04484
Pratyush Singh <pratyushsingh@utexas.edu>
2018-03-22 14:23:32 -0500

    generated a skeleton android app with basic views

---
+++ app/src/main/java/com/example/pratyushsingh/scoreapp/HomeScreen.kt
@@ -0,0 +1,14 @@
+package com.example.pratyushsingh.scoreapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+
+
+}
