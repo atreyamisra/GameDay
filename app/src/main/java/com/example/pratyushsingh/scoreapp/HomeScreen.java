adebb01387e1d31c5f1f9eb641d42898995bee34
Pratyush Singh <pratyushsingh@utexas.edu>
2018-03-22 20:58:47 -0500

    changed the kotlin file back to java for tsting

---
+++ app/src/main/java/com/example/pratyushsingh/scoreapp/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.pratyushsingh.scoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+
+    }
+
+
+}
